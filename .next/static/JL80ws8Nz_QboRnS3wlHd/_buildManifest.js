e86a6b4986388822e25d72700925e40beeb6fdb6
Fran <fran.p.21@gmail.com>
2021-04-04 17:25:28 -0300

    Remueve la carpeta .next del repositorio

--- .next/static/JL80ws8Nz_QboRnS3wlHd/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1a35f389260b08375528.css","static/chunks/pages/index-75747a5027480a41d7a1.js"],"/_error":["static/chunks/pages/_error-07e2ea8c91cf3a37282c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
